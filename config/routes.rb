e30a85186e0a3f6f17972c9753300e9b2a2319ac
jotaEmeCortat <jmcortat@gmail.com>
2025-03-15 21:08:01 -0300

    Inventory Tracking, Adding Subscribers to Products, Stock Email Notifications, Extracting a Concern, Unsubscribe links

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
   root "products#index"
+
+  resource :unsubscribe, only: [ :show ]
 end
